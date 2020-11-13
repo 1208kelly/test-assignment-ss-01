d119dcd3539d5671095b4a2fa5c57edf242a328d
1208kelly <1208kelly@gmail.com>
2020-11-13 18:34:19 +0000

    Updating js function for navbar scrolling effect

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //------- Navbar Toggler -------------------------------------------------------------------------------------------*/
 
-const mobileToggle = document.getElementsByClassName('mobile-toggle')[0]
-const navAppear = document.getElementsByClassName('nav')[0]
+const mobileToggle = document.getElementsByClassName('mobile-toggle')[0];
+const navAppear = document.getElementsByClassName('nav')[0];
 
 mobileToggle.addEventListener('click', () => {
     navAppear.classList.toggle('active')
 })
+
+
+
+//------- Navbar Shadow On Scroll -------------------------------------------------------------------------------------------*/
+
+const navShadow = document.querySelector("navbar");
+const landingPanel = document.querySelector("landing-panel");
+
+const landingPanelOptions = {}
+
+const landingPanelScroller = new IntersectionObserver(function(
+    entries, landingPanelScroller) {
+        entries.forEach(entry => {
+            console.log(entry.target);
+        })
+    }, 
+    landingPanelOptions);
+
+    landingPanelScroller.observe(landingPanel);
